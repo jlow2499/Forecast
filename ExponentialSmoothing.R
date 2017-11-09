37cae4c59bec75ec5b7c836d4abdd9434313a6c5
Jeremiah Lowhorn <jlowhorn39@yahoo.com>
2017-11-09 11:04:56 -0500

    Update ExponentialSmoothing.R

--- ExponentialSmoothing.R
+++ ExponentialSmoothing.R
@@ -77,5 +77,52 @@ autoplot(fchw)
 
 ###6 Automatic forecasting with exponential smoothing
 
+# Fit ETS model to austa in fitaus
+fitaus <- ets(austa)
 
+# Check residuals
+checkresiduals(fitaus)
 
+# Plot forecasts
+autoplot(forecast(fitaus))
+
+# Repeat for hyndsight data in fiths
+fiths <- ets(hyndsight)
+checkresiduals(fiths)
+autoplot(forecast(fiths))
+
+# Which model(s) fails test? (TRUE or FALSE)
+fitausfail <- FALSE
+fithsfail <- TRUE
+
+###7 ETS vs seasonal naive
+
+# Function to return ETS forecasts
+fets <- function(y, h) {
+  forecast(ets(y), h = h)
+}
+
+# Apply tsCV() for both methods
+e1 <- tsCV(cement, fets, h = 4)
+e2 <- tsCV(cement, snaive, h = 4)
+
+# Compute MSE of resulting errors (watch out for missing values)
+mean(e1^2, na.rm=TRUE)
+mean(e2^2, na.rm=TRUE)
+
+# Copy the best forecast MSE
+bestmse <- mean(e2^2, na.rm=TRUE)
+
+###* When does ETS fail?
+
+# Plot the lynx series
+autoplot(lynx)
+
+# Use ets() to model the lynx series
+fit <- ets(lynx)
+
+# Use summary() to look at model and parameters
+summary(fit)
+
+# Plot 20-year forecasts of the lynx series
+fit %>% forecast(h=20) %>% autoplot()
